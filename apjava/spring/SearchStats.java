1bba9f476912b5edd633e9d26cb5f279cc9a705b
emz1003 <emzhang1003@gmail.com>
2019-02-20 17:23:57 -0500

    Add latest working version of SearchStats

--- apjava/spring/SearchStats.java
+++ apjava/spring/SearchStats.java
@@ -6,28 +6,27 @@ public class SearchStats{
   private void run(){
     System.out.println("     N   | Sequential | Binary    ");
     System.out.println("----------------------------------");
-    for(int i = 1; i < 6; i++){
-      boolean isHalved = false;
+    int n = 10;
+    while (n < Math.pow(10,16)) {
       Stopwatch sw = new Stopwatch();
       Stopwatch bw = new Stopwatch();
       int[] data = new int[n];
-      int k = 0;
       for(int j = 0; j < n; j++){
-        data[j] = k;
-        if (k >= n) break;
-        else k++;
+        data[j] = j;
       }
 
-      int key = (int) (Math.random() * Math.pow(10, i));
+      int key = (int) (Math.random() * n);
       System.out.println("key: " + key);
-      System.out.println("data length" + data.length);
+      //System.out.println("data length" + data.length);
       sw.start();
-      SequentialSearchII.sequentialSearch(data, key);
+      System.out.println(SequentialSearchII.sequentialSearch(data, key));
       sw.stop();
       bw.start();
-      BinarySearchAlgorithm.binarySearch(data, key, 0, (int) (Math.pow(10, i) - 1));
+      System.out.println(BinarySearchAlgorithm.binarySearch(data, key, 0, n));
       bw.stop();
-      System.out.printf("%-10d|%10f|%10f%n", (int) Math.pow(10, i), sw.elapsedTime() * .001, bw.elapsedTime() * .001);
+      System.out.printf("%-10d|%10f|%10f%n", n, sw.elapsedTime() * .001, bw.elapsedTime() * .001);
+      if (Integer.parseInt(Integer.toString(n).substring(0, 1)) == 5) n *= 2;
+      else n *= 5;
     }
   }
 }
--- apjava/spring/SequentialSearchII.java
+++ apjava/spring/SequentialSearchII.java
@@ -3,9 +3,14 @@ public class SequentialSearchII{
   // postcondition: returns the index position of the first occurrence
   //                of x in data; -1 if not found.
   public static int sequentialSearch(int [] data, int x){
+    Stopwatch sw = new Stopwatch();
+    sw.start();
     for(int i = 0; i < data.length; i++){
-      if(data[i] == x)
+      if(data[i] == x){
+        sw.stop();
+        System.out.println("seq inside time: " + sw.elapsedTime());
         return i;
+      }
     }
     return -1;
   }
@@ -13,8 +18,4 @@ public class SequentialSearchII{
     2N + 1 comparisons, N + 1 assignments
     F(N = a(2N + 1) + b(N + 1)
   */
-
-  public static void main(String[] args){
-
-  }
 }
